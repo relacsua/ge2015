8494c2d87f3bb886b53ab017713958eef33da5c0
Muhammad Muneer <muhammad.muneer91@gmail.com>
2015-09-06 00:39:21 +0800

    add a line to fb desc

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -35,7 +35,7 @@ $(function () {
           link: 'http://sgelection2015.com/welcome',
           display: 'iframe',
           picture: 'https://s3-ap-southeast-1.amazonaws.com/ge2015/Images/icon.png',
-          description: 'GE2015 is the unofficial General Election application.'
+          description: 'GE2015 is the unofficial General Election application. Join the unofficial poll by voting.'
         },
         // callback
         function(response) {
