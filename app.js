1c65c2523f5429c566f795604a23b98e94134b91
thearith <sothearith.sreang@gmail.com>
2015-09-05 23:01:09 +0800

    make error page responsive

--- app.js
+++ app.js
@@ -121,6 +121,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
+    prevPage: req.session.lastPage,
     error: {}
   });
 });
